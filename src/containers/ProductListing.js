c9876f956b3bc9bf6d4a64c6d09409d0136b1396
timliu24 <85093613+timliu24@users.noreply.github.com>
2022-04-17 22:17:10 -0700

    added router

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+
+const ProductListing = () => {
+    const products = useSelector((state) => state);
+    console.log(products);
+    return(
+        <div className="ui grid container">
+            <ProductComponent />
+        </div>
+    );
+};
+
+export default ProductListing;
